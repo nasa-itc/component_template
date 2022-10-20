791efffe0537cc426e16bcceaeaf5465ae3767c4
Lucas, John P <John.P.Lucas@nasa.gov>
2022-10-20 14:34:32 -0400

    [nasa/nos3#92] Created generate_template.sh script;

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# component_template
+# Component Template
+
+This component template utilizes canned files from the existing [sample component](https://github.com/nasa-itc/sample) to generate a new component to do development in.
+
+Expected utilization:  
+* Determine the desired component name
+* Create a new submodule for the component via GitHub
+* Add the submodule to this project
+  * `git submodule init`
+  * `git submoudle add -f -b main <New_Submodule_Link> ./components/<New_Component_Name>`
+* Generate the new files
+  * `./generate_template.sh <New_Component_Name>`
+* Copy the new files to the submodule
+  * `mv ./new/* ../<New_Component_Name>/`
+* Commit the new files to the submodule
+  * `git add * && git add .gitignore`
+  * `git commit -m "Initial component template based on version 0.0.0"`
+* Add new component to flight software (fsw) in the following files:
+  * `./fsw/nos3_defs/cpu1_cfe_es_startup.scr`
+  * `./fsw/nos3_defs/targets.cmake`
+  * `./fsw/nos3_defs/tables/*`
+* Add new component to ground software (gsw) in the following files:
+  * `./gsw/cosmos/system/nos3_system.txt`
+  * `./gsw/cosmos/tools/nos3_cmd_tlm_server.txt`
+  * `./gsw/scripts/launch.sh`
---
+++ generate_template.sh
@@ -0,0 +1,57 @@
+#!/usr/bin/env bash
+#
+# Generate a new component by:
+#   Copy all files in "code" to "new" directory
+#   Change "./new/sample" to the provided component name
+#   Change the "./new/sample" filenames to the provided component name
+#
+
+SCRIPT_DIR=$(cd `dirname $0` && pwd)
+CODE_DIR=$SCRIPT_DIR/code
+NEW_DIR=$SCRIPT_DIR/new
+
+# Debugging
+echo ""
+echo "Script directory = " $SCRIPT_DIR
+echo "Code directory = " $CODE_DIR
+echo "New directory = " $NEW_DIR
+echo ""
+
+
+# Check argument
+if [ "$#" -ne 1 ]; then
+    echo "Illegal number of parameters!"
+    echo "  Expected utilization:"
+    echo "    ./generate_template.sh <New_Component_Name>"
+    echo ""
+    exit 0
+fi
+COMPONENT_NAME=$1
+COMPONENT_NAME_LOWER=$(echo "$COMPONENT_NAME" | sed -e 's/\(.*\)/\L\1/')
+COMPONENT_NAME_FIRST=${COMPONENT_NAME_LOWER^}
+COMPONENT_NAME_UPPER=${COMPONENT_NAME_LOWER^^}
+echo "New component name = " $COMPONENT_NAME
+echo "New component name lower = " $COMPONENT_NAME_LOWER
+echo "New component name first = " $COMPONENT_NAME_FIRST
+echo "New component name upper = " $COMPONENT_NAME_UPPER
+echo ""
+
+echo "Copy all files in ./code to ./new directory"
+cp -R ./code ./new
+echo ""
+
+echo "Change ./new/sample to the provided component name"
+find ./new -type f -exec sed -i "s/SAMPLE/$COMPONENT_NAME_UPPER/g" {} \;
+find ./new -type f -exec sed -i "s/Sample/$COMPONENT_NAME_FIRST/g" {} \;
+find ./new -type f -exec sed -i "s/sample/$COMPONENT_NAME_LOWER/g" {} \;
+echo ""
+
+echo "Change the ./new/sample filenames to the provided component name"
+mv ./new/gsw/SAMPLE ./new/gsw/$COMPONENT_NAME_UPPER
+find ./new -type f | sed "p;s/SAMPLE/$COMPONENT_NAME_UPPER/g" | xargs -d '\n' -n 2 mv 2> /dev/null
+find ./new -type f | sed "p;s/Sample/$COMPONENT_NAME_FIRST/g" | xargs -d '\n' -n 2 mv 2> /dev/null
+find ./new -type f | sed "p;s/sample/$COMPONENT_NAME_LOWER/g" | xargs -d '\n' -n 2 mv 2> /dev/null
+echo ""
+
+echo "generate_template.sh complete!"
+echo ""
